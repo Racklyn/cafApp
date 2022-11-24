90d8fce62eb922d6161c86f7f6f0d3810e0209f5
Jucyelle <jucyellebnascimento@gmail.com>
2022-11-24 17:03:18 -0300

    iniciando estrutura

---
+++ src/pages/Final/Final.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './final.css';
+import LogoImg from '../../assets/logo-name.png';
+import {DownloadSimple, HouseSimple} from 'phosphor-react';
+
+const Final = () => {
+  return (
+    <body>
+      <div className='container'>
+        <div className='box'>
+          <img src={LogoImg} alt='CafApp' style={{width: 136, height:161}}/>
+          <p style={{marginTop: 0, marginBottom: 12}}>O cafApp agradece o seu pedido.</p>
+          <div>
+            <button><DownloadSimple size={24} weight="bold" color='#D2C1B0'/></button>
+            <button style={{marginLeft: 6}}><HouseSimple size={24} weight="bold" color='#D2C1B0'/></button>
+          </div>
+        </div>
+      </div>
+    </body>
+  );
+}
+
+export default Final;
