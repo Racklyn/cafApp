d7fc71a2d3a4eb07236c28b8e76a2adccd12f45c
Racklyn Sotero <racklynsotero@gmail.com>
2022-11-25 23:45:24 -0300

    Criando principais classes do sistema

---
+++ src/model/Flavor.ts
@@ -0,0 +1,11 @@
+export default class Flavor{
+    title: String
+    description: String
+    //image: 
+
+    constructor (title: String, description: String){
+        this.title = title
+        this.description = description
+        //
+    }
+}
